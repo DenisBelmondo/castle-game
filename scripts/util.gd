c5b43f9696e23344a794656a422a14106b3a8baa
DenisBelmondo <steakwilsomusic@gmail.com>
2025-07-25 01:19:43 -0600

    add comments to util.gd

--- scripts/util.gd
+++ scripts/util.gd
@@ -4,6 +4,9 @@
 #
 
 
+# [TODO]: is this necessary?
+## Safely chooses between calling `parent.add_child` or `child.reparent`
+## depending on whether or not it's in the tree.
 static func reparent_or_add_child(child: Node, parent: Node) -> void:
 	var f := parent.add_child.bind(child)
 
@@ -13,25 +16,16 @@ static func reparent_or_add_child(child: Node, parent: Node) -> void:
 	f.call()
 
 
-static func _find_children(results: Array[Node], node: Node, predicate: Callable, recursive: bool = true, owned: bool = true) -> Array[Node]:
-	for c in node.get_children():
-		if owned and not is_instance_valid(c.owner):
-			continue
-
-		if predicate.call(c) == true:
-			results.append(c)
-
-		if recursive:
-			_find_children(results, c, predicate, recursive, owned)
-
-	return results
-
-
+## Recursively finds children of a node as long as they satisfy `predicate`
+## (a callable taking in a child node and returning a boolean based on some
+## condition).
 static func find_children(node: Node, predicate: Callable, recursive: bool = true, owned: bool = true) -> Array[Node]:
 	var results: Array[Node]
 	return _find_children(results, node, predicate, recursive, owned)
 
 
+## A wrapper for `PhysicsDirectSpaceState3D.intersect_ray` that allows users to
+## query multiple hits along a ray.
 static func intersect_ray_all_3d(direct_space_state: PhysicsDirectSpaceState3D, params: PhysicsRayQueryParameters3D, max_results: int = 32) -> Array[Dictionary]:
 	var results: Array[Dictionary] = []
 	var exclude: Array[RID] = params.exclude.duplicate()
@@ -65,3 +59,22 @@ static func intersect_ray_all_3d(direct_space_state: PhysicsDirectSpaceState3D, 
 static func filter_physics_query_by_object(physics_query_results: Array, exclude: Array[Object], result_object_field_name: StringName = &'collider') -> Array:
 	return physics_query_results.filter(func (v) -> bool:
 			return v[result_object_field_name] not in exclude)
+
+
+#
+# internal
+#
+
+
+static func _find_children(results: Array[Node], node: Node, predicate: Callable, recursive: bool = true, owned: bool = true) -> Array[Node]:
+	for c in node.get_children():
+		if owned and not is_instance_valid(c.owner):
+			continue
+
+		if predicate.call(c) == true:
+			results.append(c)
+
+		if recursive:
+			_find_children(results, c, predicate, recursive, owned)
+
+	return results
